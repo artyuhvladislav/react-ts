81c8cdd6db1a610f57699992d3e20b5855216944
artyuhvladislav <workto22@gmail.com>
2023-03-18 12:18:27 +0100

    delete use client in location page episode page

--- app/episode/[id]/page.tsx
+++ app/episode/[id]/page.tsx
@@ -1,16 +1,4 @@
-'use client';
-// import { useRouter } from 'next/router';
-
-const fetchCharacter = async () => {
-  // const router = useRouter();
-  // const { page } = router.query;
-  // console.log(page);
-  // const res = await fetch(`https://rickandmortyapi.com/api/character/?page=${page}`);
-  // return await res.json();
-};
-
 const CharacterPage = async () => {
-  const character = await fetchCharacter();
   return <section>{111}</section>;
 };
 
